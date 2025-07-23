18c2651978cffbf63979709922a539c8e254302e
Th3Ward3n <liam.hatton@gmail.com>
2025-07-23 15:25:16 -0400

    Alligned pieces on board

--- src/components/ChessPiece.vue
+++ src/components/ChessPiece.vue
@@ -26,41 +26,48 @@ type PieceInfo = {
 type AddInit<Type extends { [k: string]: any }> = { [Key in keyof Type as `init${Capitalize<Key & string>}`]: Type[Key] };
 type InitPieceInfo = Partial<AddInit<PieceInfo>>;
 // type InitPieceInfo = AddInit<PieceInfo>;
-const { 
-    initColour = "Black", 
-    initType = "P", 
-    // @ts-ignore
-    initX = 0, 
-    // @ts-ignore
-    initY = 0 
-} = defineProps<InitPieceInfo>();
+// @ts-ignore
+// const { 
+//     // @ts-ignore
+//     initColour = "Black", 
+//     // @ts-ignore
+//     initType = "P", 
+//     // @ts-ignore
+//     initX = 0, 
+//     // @ts-ignore
+//     initY = 0 
+// } = 
+defineProps<InitPieceInfo>();
 const colour = ref<PieceInfo["colour"]>("Black"), 
     type = ref<PieceInfo["type"]>("P"), 
     x = ref<PieceInfo["x"]>(0),
     y = ref<PieceInfo["y"]>(0);
 
 // const coords = [x, y] as const;
-
+// @ts-ignore
 const notatePosition = () => `${String.fromCharCode(65 + Math.abs(x.value - 7))}${y.value + 1}`;
 // @ts-ignore
 const buildStyleClass = () => `piece ${colour.value.substring(0, 1).toLowerCase()}${type.value.toLowerCase()} sqr-${x.value + 1}${y.value + 1}`;
+// @ts-ignore
 const typeIndex = ["P", "B", "N", "R", "K", "Q"] as const;
 </script>
 
 <template>
     <div>
-        <!-- {{ initColour }} {{ initType }} [{{ initX }}, {{ initY }}] -->
-        <!-- <div :class="initStyle"></div> -->
-        <h1>Piece Details</h1>
-        <div>
-            <p>
-                Colour: <button @click="colour = colour === 'White' ? 'Black' : 'White'">{{ colour ?? initColour }}</button> 
-                || 
-                Type: <button @click="type = typeIndex.indexOf(type) === 5 ? typeIndex[0] : typeIndex[typeIndex.indexOf(type) + 1]">{{ type ?? initType }}</button>
-                ||
-                Position: <button>{{ notatePosition() }}</button>
-            </p>
-        </div>
+        <!-- <div v-if="!$props.initX && !$props.initY"> -->
+            <!-- {{ initColour }} {{ initType }} [{{ initX }}, {{ initY }}] -->
+            <!-- <div :class="initStyle"></div> -->
+            <!-- <h1>Piece Details</h1>
+            <div>
+                <p>
+                    Colour: <button @click="colour = colour === 'White' ? 'Black' : 'White'">{{ colour ?? initColour }}</button> 
+                    || 
+                    Type: <button @click="type = typeIndex.indexOf(type) === 5 ? typeIndex[0] : typeIndex[typeIndex.indexOf(type) + 1]">{{ type ?? initType }}</button>
+                    ||
+                    Position: <button>{{ notatePosition() }}</button>
+                </p>
+            </div>
+        </div> -->
     </div>
 </template>
 
@@ -96,5 +103,32 @@ const typeIndex = ["P", "B", "N", "R", "K", "Q"] as const;
 .sqr-61 { transform: translate(500%, 700%); }
 .sqr-71 { transform: translate(600%, 700%); }
 .sqr-81 { transform: translate(700%, 700%); }
+
+.sqr-12 { transform: translateY(600%); }
+.sqr-22 { transform: translate(100%, 600%); }
+.sqr-32 { transform: translate(200%, 600%); }
+.sqr-42 { transform: translate(300%, 600%); }
+.sqr-52 { transform: translate(400%, 600%); }
+.sqr-62 { transform: translate(500%, 600%); }
+.sqr-72 { transform: translate(600%, 600%); }
+.sqr-82 { transform: translate(700%, 600%); }
+
+.sqr-17 { transform: translateY(100%); }
+.sqr-27 { transform: translate(100%, 100%); }
+.sqr-37 { transform: translate(200%, 100%); }
+.sqr-47 { transform: translate(300%, 100%); }
+.sqr-57 { transform: translate(400%, 100%); }
+.sqr-67 { transform: translate(500%, 100%); }
+.sqr-77 { transform: translate(600%, 100%); }
+.sqr-87 { transform: translate(700%, 100%); }
+
+.sqr-18 { transform: translate(); }
+.sqr-28 { transform: translate(100%); }
+.sqr-38 { transform: translate(200%); }
+.sqr-48 { transform: translate(300%); }
+.sqr-58 { transform: translate(400%); }
+.sqr-68 { transform: translate(500%); }
+.sqr-78 { transform: translate(600%); }
+.sqr-88 { transform: translate(700%); }
 
 </style>
