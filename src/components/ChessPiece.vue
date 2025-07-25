8edc2fa74e861720394c5a08e81ae07be3426b70
Th3Ward3n <liam.hatton@gmail.com>
2025-07-25 19:52:51 -0400

    Added positions and movement rules for pieces, as well as inverted placement

--- src/components/ChessPiece.vue
+++ src/components/ChessPiece.vue
@@ -12,13 +12,14 @@
  */
 -->
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, useTemplateRef } from 'vue';
 
 type PieceInfo = {
     colour: "White" | "Black";
     type: "P" | "B" | "N" | "R" | "K" | "Q";
     x: number;
     y: number;
+    inverted: boolean;
 };
 /** @vue-ignore */
 type AddInit<Type extends { [k: string]: any }> = { [Key in keyof Type as `init${Capitalize<Key & string>}`]: Type[Key] };
@@ -36,25 +37,77 @@ type InitPieceInfo = Partial<AddInit<PieceInfo>>;
 //     initY = 0 
 // } = 
 defineProps<InitPieceInfo>();
-const colour = ref<PieceInfo["colour"]>("Black"), 
-    type = ref<PieceInfo["type"]>("P"), 
-    x = ref<PieceInfo["x"]>(0),
-    y = ref<PieceInfo["y"]>(0);
+// const colour = ref<PieceInfo["colour"]>("Black");
+    // type = ref<PieceInfo["type"]>("P"), 
+    // x = ref<PieceInfo["x"]>(0),
+    // y = ref<PieceInfo["y"]>(0);
 
 // @ts-ignore
 const hasMoved = ref<boolean>(false);
 
+
 // const coords = [x, y] as const;
 // @ts-ignore
-const notatePosition = () => `${String.fromCharCode(65 + Math.abs(x.value - 7))}${y.value + 1}`;
+// const notatePosition = () => `${String.fromCharCode(65 + Math.abs(x.value - 7))}${y.value + 1}`;
+const buildNotation = (
+    x: number, 
+    y: number, 
+    isInverted: boolean = true
+) => `${String.fromCharCode(65 + ((isInverted) ? x - 1 : Math.abs(x - 8)))}${(!isInverted) ? Math.abs(y - 8) : y}`;
 // @ts-ignore
-const buildStyleClass = () => `piece ${colour.value.substring(0, 1).toLowerCase()}${type.value.toLowerCase()} sqr-${x.value + 1}${y.value + 1}`;
+// const buildStyleClass = () => `piece ${colour.value.substring(0, 1).toLowerCase()}${type.value.toLowerCase()} sqr-${x.value + 1}${y.value + 1}`;
 // @ts-ignore
 const typeIndex = ["P", "B", "N", "R", "K", "Q"] as const;
 
-const yInvert = (team: PieceInfo["colour"], mag: number/**, isFlipped: boolean = false */) => {
-    if (team === "Black") return mag * -1;
-    else return mag * +1;
+const pieceInstance = useTemplateRef("piece-instance");
+
+interface ActivePieceDetails {
+    c: "White" | "Black";
+    t: typeof typeIndex[number];
+    x: number;
+    y: number;
+    inv: boolean;
+}
+
+const getPieceDetails = (): ActivePieceDetails => {
+    if (!pieceInstance.value) throw new Error("[WARNING]: Selected Piece is missing root element!!");
+    const pieceColour = pieceInstance.value.getAttribute("init-colour") as "White" | "Black";
+    const pieceType = pieceInstance.value.getAttribute("init-type") as typeof typeIndex[number];
+    const pieceX = parseInt(pieceInstance.value.getAttribute("init-x") as `${number}`);
+    const pieceY = parseInt(pieceInstance.value.getAttribute("init-y") as `${number}`);
+    const isInverted = Boolean(pieceInstance.value.getAttribute("init-inverted"));
+    return { c: pieceColour, t: pieceType, x: pieceX, y: pieceY, inv: isInverted };
+};
+
+const validMove = () => {
+    const details = getPieceDetails();
+    // displayRules(details.c, details.t);
+    const moveList = buildActionList(details, "move");
+    // EMIT EVENT TO CONTROLLER FOR SHOWING MOVEMENT HINTS!!
+    moveList.forEach((move) => console.log(`${details.c} ${details.t} @ ${buildNotation(details.x, details.y, details.inv)}: `, buildNotation(move.x, move.y, details.inv)));
+    // console.log(moveList);
+};
+
+const buildActionList = (details: ActivePieceDetails, actionType: "move" | "attack" | "special" = "move") => {
+    const baseActions = ruleSet[details.t][actionType] as { x: number, y: number, inverter?: typeof yInvert }[];
+    if (!baseActions.length) return [{x: 1, y: 1}];
+    const validActions: { x: number, y: number }[] = [];
+    for (const action of baseActions) {
+        const trueY = (action.inverter && typeof action.inverter === 'function') ? action.inverter(details.c, action.y, details.inv) : action.y;
+        validActions.push({
+            x: details.x + action.x,
+            y: details.y + trueY
+        });
+    }
+    const isValidPosition = ({ x, y }: { x: number, y: number}) => x >= 1 && x <= 8 && y >= 1 && y <= 8;
+    return validActions.filter(isValidPosition);
+};
+
+// const applyInverter = ()
+
+const yInvert = (team: PieceInfo["colour"], mag: number, isInverted: boolean = false ) => {
+    if (team === "Black") return (isInverted) ? mag * -1 : mag * 1;
+    else return (isInverted) ? mag * 1 : mag * -1;
 
     // switch (isFlipped) {
     //     // POV Black
@@ -72,42 +125,40 @@ const yInvert = (team: PieceInfo["colour"], mag: number/**, isFlipped: boolean =
 const ruleSet = {
     "P": {
         "move": [
-            { x: x.value, y: y.value + yInvert(colour.value, 1) }
+            { x: 0, y: 1, inverter: yInvert }
         ],
         "attack": [
-            { x: x.value + 1, y: y.value + yInvert(colour.value, 1) },
-            { x: x.value - 1, y: y.value + yInvert(colour.value, 1) }
+            { x: 0 + 1, y: 1, inverter: yInvert },
+            { x: 0 - 1, y: 1, inverter: yInvert }
         ],
         "special": [],
     },
     "N": {
         /**      8   1
          *       < ^ >
-         *         ^
-         * 7 ^     ^     ^ 2
-         *   < < < N > > >
-         * 6 v     v     v 3
-         *         v
+         *   7 ^   ^   ^ 2
+         *     < < N > >
+         *   6 v   v   v 3
          *       < v > 
          *       5   4
          */
         "move": [
             // 1
-            { x: x.value + 1, y: y.value + 3 },
+            { x: 0 + 1, y: 0 + 2 },
             // 2
-            { x: x.value + 3, y: y.value + 1 },
+            { x: 0 + 2, y: 0 + 1 },
             // 3
-            { x: x.value + 3, y: y.value - 1 },
+            { x: 0 + 2, y: 0 - 1 },
             // 4
-            { x: x.value + 1, y: y.value - 3 },
+            { x: 0 + 1, y: 0 - 2 },
             // 5
-            { x: x.value - 1, y: y.value - 3 },
+            { x: 0 - 1, y: 0 - 2 },
             // 6
-            { x: x.value - 3, y: y.value - 1 },
+            { x: 0 - 2, y: 0 - 1 },
             // 7
-            { x: x.value - 3, y: y.value + 1 },
+            { x: 0 - 2, y: 0 + 1 },
             // 8
-            { x: x.value - 1, y: y.value + 3 },
+            { x: 0 - 1, y: 0 + 2 },
         ],
         "attack": [],
         "special": [],
@@ -134,14 +185,31 @@ const ruleSet = {
     }
 };
 
+// @ts-ignore
 const displayRules = (team: PieceInfo['colour'], pieceType: PieceInfo['type']) => {
     console.log("Team Selected: ", team);
     console.log("Moveset for selected piece: ", ...ruleSet[pieceType]['move']);
 };
+
+// onMounted(() => {
+//     // console.log(pieceInstance.value);
+//     if (!pieceInstance.value) return;
+//     // const pieceCode = pieceInstance.value.className.split(" ")[1] as `${"w" | "b"}${Lowercase<typeof typeIndex[number]>}`;
+//     // console.log(pieceCode);
+    
+//     const pieceColour = pieceInstance.value.getAttribute("init-colour") as "White" | "Black";
+//     const pieceType = pieceInstance.value.getAttribute("init-type") as typeof typeIndex[number];
+//     console.log(pieceColour, pieceType);
+
+//     displayRules(pieceColour, pieceType);
+// });
 </script>
 
 <template>
-    <div @click="displayRules($props.initColour ?? 'Black', $props.initType ?? 'P')">
+    <div 
+        @click="validMove()"
+        ref="piece-instance"
+    >
         <!-- 
             Emit Event??
             Build moveset map overlay?
@@ -174,6 +242,19 @@ const displayRules = (team: PieceInfo['colour'], pieceType: PieceInfo['type']) =
     top: 0;
     position: absolute;
     will-change: transform;
+}
+
+.hint {
+    background-color: rgba(0, 0, 0, .14);
+    background-clip: content-box;
+    border-radius: 50%;
+    box-sizing: border-box;
+    padding: 4.2%;
+    height: 12.5%;
+    width: 12.5%;
+    left: 0;
+    top: 0;
+    position: absolute;
 }
 
 .piece:hover {
@@ -215,6 +296,42 @@ const displayRules = (team: PieceInfo['colour'], pieceType: PieceInfo['type']) =
 .sqr-72 { transform: translate(600%, 600%); }
 .sqr-82 { transform: translate(700%, 600%); }
 
+.sqr-13 { transform: translateY(500%); }
+.sqr-23 { transform: translate(100%, 500%); }
+.sqr-33 { transform: translate(200%, 500%); }
+.sqr-43 { transform: translate(300%, 500%); }
+.sqr-53 { transform: translate(400%, 500%); }
+.sqr-63 { transform: translate(500%, 500%); }
+.sqr-73 { transform: translate(600%, 500%); }
+.sqr-83 { transform: translate(700%, 500%); }
+
+.sqr-14 { transform: translateY(400%); }
+.sqr-24 { transform: translate(100%, 400%); }
+.sqr-34 { transform: translate(200%, 400%); }
+.sqr-44 { transform: translate(300%, 400%); }
+.sqr-54 { transform: translate(400%, 400%); }
+.sqr-64 { transform: translate(500%, 400%); }
+.sqr-74 { transform: translate(600%, 400%); }
+.sqr-84 { transform: translate(700%, 400%); }
+
+.sqr-15 { transform: translateY(300%); }
+.sqr-25 { transform: translate(100%, 300%); }
+.sqr-35 { transform: translate(200%, 300%); }
+.sqr-45 { transform: translate(300%, 300%); }
+.sqr-55 { transform: translate(400%, 300%); }
+.sqr-65 { transform: translate(500%, 300%); }
+.sqr-75 { transform: translate(600%, 300%); }
+.sqr-85 { transform: translate(700%, 300%); }
+
+.sqr-16 { transform: translateY(200%); }
+.sqr-26 { transform: translate(100%, 200%); }
+.sqr-36 { transform: translate(200%, 200%); }
+.sqr-46 { transform: translate(300%, 200%); }
+.sqr-56 { transform: translate(400%, 200%); }
+.sqr-66 { transform: translate(500%, 200%); }
+.sqr-76 { transform: translate(600%, 200%); }
+.sqr-86 { transform: translate(700%, 200%); }
+
 .sqr-17 { transform: translateY(100%); }
 .sqr-27 { transform: translate(100%, 100%); }
 .sqr-37 { transform: translate(200%, 100%); }
@@ -232,5 +349,42 @@ const displayRules = (team: PieceInfo['colour'], pieceType: PieceInfo['type']) =
 .sqr-68 { transform: translate(500%); }
 .sqr-78 { transform: translate(600%); }
 .sqr-88 { transform: translate(700%); }
+
+/** INVERTED */
+.inv.sqr-11 { transform: translate(700%); }
+.inv.sqr-21 { transform: translate(600%); }
+.inv.sqr-31 { transform: translate(500%); }
+.inv.sqr-41 { transform: translate(400%); }
+.inv.sqr-51 { transform: translate(300%); }
+.inv.sqr-61 { transform: translate(200%); }
+.inv.sqr-71 { transform: translate(100%); }
+.inv.sqr-81 { transform: translateY(0%); }
+
+.inv.sqr-12 { transform: translate(700%, 100%); }
+.inv.sqr-22 { transform: translate(600%, 100%); }
+.inv.sqr-32 { transform: translate(500%, 100%); }
+.inv.sqr-42 { transform: translate(400%, 100%); }
+.inv.sqr-52 { transform: translate(300%, 100%); }
+.inv.sqr-62 { transform: translate(200%, 100%); }
+.inv.sqr-72 { transform: translate(100%, 100%); }
+.inv.sqr-82 { transform: translateY(100%); }
+
+.inv.sqr-17 { transform: translate(700%, 600%); }
+.inv.sqr-27 { transform: translate(600%, 600%); }
+.inv.sqr-37 { transform: translate(500%, 600%); }
+.inv.sqr-47 { transform: translate(400%, 600%); }
+.inv.sqr-57 { transform: translate(300%, 600%); }
+.inv.sqr-67 { transform: translate(200%, 600%); }
+.inv.sqr-77 { transform: translate(100%, 600%); }
+.inv.sqr-87 { transform: translateY(600%); }
+
+.inv.sqr-18 { transform: translate(700%, 700%); }
+.inv.sqr-28 { transform: translate(600%, 700%); }
+.inv.sqr-38 { transform: translate(500%, 700%); }
+.inv.sqr-48 { transform: translate(400%, 700%); }
+.inv.sqr-58 { transform: translate(300%, 700%); }
+.inv.sqr-68 { transform: translate(200%, 700%); }
+.inv.sqr-78 { transform: translate(100%, 700%); }
+.inv.sqr-88 { transform: translateY(700%); }
 
 </style>
--- src/components/GameController.vue
+++ src/components/GameController.vue
@@ -9,41 +9,93 @@ const pieceInitData: [
     coords: [x: number, y: number],
     classStyle: string
 ][] = [
-    ["White", "P", [0, 6], "piece wp sqr-17"],
-    ["White", "P", [1, 6], "piece wp sqr-27"],
-    ["White", "P", [2, 6], "piece wp sqr-37"],
-    ["White", "P", [3, 6], "piece wp sqr-47"],
-    ["White", "P", [4, 6], "piece wp sqr-57"],
-    ["White", "P", [5, 6], "piece wp sqr-67"],
-    ["White", "P", [6, 6], "piece wp sqr-77"],
-    ["White", "P", [7, 6], "piece wp sqr-87"],
-    ["White", "N", [1, 7], "piece wn sqr-28"],
-    ["White", "N", [6, 7], "piece wn sqr-78"],
-    ["White", "B", [2, 7], "piece wb sqr-38"],
-    ["White", "B", [5, 7], "piece wb sqr-68"],
-    ["White", "R", [0, 7], "piece wr sqr-18"],
-    ["White", "R", [7, 7], "piece wr sqr-88"],
-    ["White", "K", [4, 7], "piece wk sqr-58"],
-    ["White", "Q", [3, 7], "piece wq sqr-48"],
-    ["Black", "P", [0, 2], "piece bp sqr-12"],
-    ["Black", "P", [1, 2], "piece bp sqr-22"],
-    ["Black", "P", [2, 2], "piece bp sqr-32"],
-    ["Black", "P", [3, 2], "piece bp sqr-42"],
-    ["Black", "P", [4, 2], "piece bp sqr-52"],
-    ["Black", "P", [5, 2], "piece bp sqr-62"],
-    ["Black", "P", [6, 2], "piece bp sqr-72"],
-    ["Black", "P", [7, 2], "piece bp sqr-82"],
-    ["Black", "N", [1, 0], "piece bn sqr-21"],
-    ["Black", "N", [6, 0], "piece bn sqr-71"],
-    ["Black", "B", [2, 0], "piece bb sqr-31"],
-    ["Black", "B", [5, 0], "piece bb sqr-61"],
-    ["Black", "R", [0, 0], "piece br sqr-11"],
-    ["Black", "R", [7, 0], "piece br sqr-81"],
-    ["Black", "K", [4, 0], "piece bk sqr-51"],
-    ["Black", "Q", [3, 0], "piece bq sqr-41"],
+    ["Black", "P", [1, 7], "piece bp sqr-17"],
+    ["Black", "P", [2, 7], "piece bp sqr-27"],
+    ["Black", "P", [3, 7], "piece bp sqr-37"],
+    ["Black", "P", [4, 7], "piece bp sqr-47"],
+    ["Black", "P", [5, 7], "piece bp sqr-57"],
+    ["Black", "P", [6, 7], "piece bp sqr-67"],
+    ["Black", "P", [7, 7], "piece bp sqr-77"],
+    ["Black", "P", [8, 7], "piece bp sqr-87"],
+    ["Black", "N", [2, 8], "piece bn sqr-28"],
+    ["Black", "N", [7, 8], "piece bn sqr-78"],
+    ["Black", "B", [3, 8], "piece bb sqr-38"],
+    ["Black", "B", [6, 8], "piece bb sqr-68"],
+    ["Black", "R", [1, 8], "piece br sqr-18"],
+    ["Black", "R", [8, 8], "piece br sqr-88"],
+    ["Black", "K", [5, 8], "piece bk sqr-58"],
+    ["Black", "Q", [4, 8], "piece bq sqr-48"],
+    ["White", "P", [1, 2], "piece wp sqr-12"],
+    ["White", "P", [2, 2], "piece wp sqr-22"],
+    ["White", "P", [3, 2], "piece wp sqr-32"],
+    ["White", "P", [4, 2], "piece wp sqr-42"],
+    ["White", "P", [5, 2], "piece wp sqr-52"],
+    ["White", "P", [6, 2], "piece wp sqr-62"],
+    ["White", "P", [7, 2], "piece wp sqr-72"],
+    ["White", "P", [8, 2], "piece wp sqr-82"],
+    ["White", "N", [2, 1], "piece wn sqr-21"],
+    ["White", "N", [7, 1], "piece wn sqr-71"],
+    ["White", "B", [3, 1], "piece wb sqr-31"],
+    ["White", "B", [6, 1], "piece wb sqr-61"],
+    ["White", "R", [1, 1], "piece wr sqr-11"],
+    ["White", "R", [8, 1], "piece wr sqr-81"],
+    ["White", "K", [5, 1], "piece wk sqr-51"],
+    ["White", "Q", [4, 1], "piece wq sqr-41"],
 ];
 
-import { useTemplateRef } from 'vue';
+type PieceInitData = [
+    colour: "White" | "Black",
+    type: "P" | "N" | "B" | "R" | "K" | "Q",
+    coords: [x: number, y: number],
+    classStyle: string
+];
+type PartialPieceInitData = [PieceInitData[0], PieceInitData[1], PieceInitData[2]];
+
+const buildPieceClass = (
+    c: PieceInitData[0], 
+    t: PieceInitData[1], 
+    coords: PieceInitData[2],
+    inverted: boolean
+) => `piece ${c.charAt(0).toLowerCase()}${t.toLowerCase()} sqr-${coords[0]}${coords[1]} ${(inverted) ? "inv" : ""}`;
+
+// const invertCoords = (
+//     c: PieceInitData[0],
+//     t: PieceInitData[1],
+//     coords: PieceInitData[2],
+//     ..._: any
+// ) => {
+//     const invertedX = coords[0]; // (coords[0] + (4 - ((coords[0] + 1) / 2))) % 8
+
+//     // const yOffset = (c === "White") 
+//     const invertedY = Math.abs(coords[1] + ((c === "White") ? -8 : )); // Math.abs(coords[1] + invertAs)
+
+//     const pawnOffset = (t !== "P") ? 0 : (c === "White") ? 0 : -2;
+//     const invertAs = (c === 'White') ? -7 + pawnOffset : 7 + pawnOffset;
+
+//     return [c, t, [invertedX, invertedY]] as PartialPieceInitData;
+// };
+
+const buildInitData = (inverted: boolean = false) => {
+    const initDataCopy = pieceInitData.slice();
+    return initDataCopy.reduce<PieceInitData[]>((acc, data) => {
+        const partialData = [data[0], data[1], data[2]] as PartialPieceInitData;
+        acc.push([...partialData, buildPieceClass(...[...partialData, inverted])]);
+        return acc;
+    }, []);
+    // return (!inverted) 
+    //     ? initDataCopy.reduce<PieceInitData[]>((acc, data) => {
+    //         const invertedData = invertCoords(...data);
+    //         acc.push([...invertedData, buildPieceClass(...invertedData)]);
+    //         return acc;
+    //     }, [])
+    //     : initDataCopy.reduce<PieceInitData[]>((acc, data) => {
+    //         const partialData = [data[0], data[1], data[2]] as const;
+    //         acc.push([...partialData, buildPieceClass(...partialData)]);
+    //         return acc;
+    //     }, []);
+};
+
+import { ref, useTemplateRef, watch } from 'vue';
 
 const canvasBoard = useTemplateRef<HTMLCanvasElement>("game-board");
 
@@ -66,7 +118,7 @@ const initCanvas = () => {
         c: cRsp
     };
 };
-const loadGameBoard = () => {
+const loadGameBoard = (inverted: boolean = false) => {
     const { ctx, c } = initCanvas();
     // 8 x 8
     const DIM = 8;
@@ -81,7 +133,8 @@ const loadGameBoard = () => {
      * x = 7, y = 7 === H1
      */
     for (let i = 0; i < DIM; i++) for (let j = 0; j < DIM; j++) {
-        // Alternating colours
+        // Alternating colours 
+        // USE && inverted
         ctx.fillStyle = ((j + i) % 2 === 0) ? "White" : "Black";
         // console.log(`Current Style @ ${i}${String.fromCharCode(65 + j)}: ${ctx.fillStyle}`);
         ctx.fillRect(j * w, i * h, w, h);
@@ -90,14 +143,21 @@ const loadGameBoard = () => {
         if (j !== 0 && i !== 7) continue;
         ctx.fillStyle = (ctx.fillStyle === "#ffffff") ? "Black": "White";
         ctx.font = "bold 1.8rem serif";
-        if (j === 0) ctx.fillText(`${Math.abs(i - 8)}`, qw * 0.5 + (j * w), qh * 0.9 + (i * h), qw);
-        if (i === 7) ctx.fillText(`${String.fromCharCode(65 + j)}`, qw * 3.0 + (j * w), qh * 3.5 + (i * h), qw);
+        if (j === 0) ctx.fillText(`${(!inverted) ? Math.abs(i - 8) : i + 1}`, qw * 0.5 + (j * w), qh * 0.9 + (i * h), qw);
+        if (i === 7) ctx.fillText(`${String.fromCharCode(65 + ((!inverted) ? j : Math.abs(j - 7)))}`, qw * 3.0 + (j * w), qh * 3.5 + (i * h), qw);
         // if (!pieceDetails) continue;
         // ctx.font = "bold 20px serif";
         // const pTxt = `${pieceDetails.c.split("")[0]}:${pieceDetails.t}`;
         // ctx.fillText(pTxt, qw * 2 + (j * w), qh * 2 + (i * h), qw);
     }
 };
+
+const isInverted = ref<boolean>(false);
+
+watch(isInverted, (newInv, oldInv) => {
+    if (newInv === oldInv) return;
+    loadGameBoard(newInv);
+});
 
 // const isStartingPosition = (coords: [x: number, y: number]) => coords[1] > 3 || coords[1] < 5;
 // const drawPiece = (x: number, y: number) => {
@@ -124,14 +184,16 @@ const loadGameBoard = () => {
 </script>
 
 <template>
-    <button @click="loadGameBoard">Load Board!</button>
+    <button @click="loadGameBoard(isInverted)">Load Board!</button>
+    <button @click="isInverted = !isInverted">Switch Team! Current Team: {{ !isInverted ? "White" : "Black" }}</button>
     <div class="game-cont">
         <canvas ref="game-board"></canvas> <!-- height="500" width="500" -->
-        <chess-piece v-for="[c, t, coords, style] in pieceInitData"
+        <chess-piece v-for="[c, t, coords, style] in buildInitData(isInverted)"
             :init-colour="c"
             :init-type="t"
             :init-x=coords[0]
             :init-y=coords[1]
+            :init-inverted=isInverted
             :class=style
         ></chess-piece>
     </div>
